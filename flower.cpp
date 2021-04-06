74f0b05961a694114932f4f82dc0f6bdcdf260cf
laletinivan <laletinivan@users.noreply.github.com>
2021-04-06 19:23:41 +0700

    flower pog fix

--- flower.cpp
+++ flower.cpp
@@ -4,10 +4,33 @@ namespace plants {
 	void flower::InData(ifstream& ifst) {
 		int tmp;
 		ifst >> tmp;
+		pog = plant::place_of_growth(tmp);
+		ifst >> tmp;
 		t = flower::type(tmp);
 	}
 	void flower::Out(ofstream& ofst) {
 		ofst << "Name is " << name << " ";
+		ofst << "place of growth is ";
+		switch (pog) {
+		case Tundra:
+			ofst << "Tundra ";
+			break;
+		case Taiga:
+			ofst << "Taiga ";
+			break;
+		case Forest:
+			ofst << "Forest ";
+			break;
+		case Steppe:
+			ofst << "Steppe ";
+			break;
+		case Jungle:
+			ofst << "Jungle ";
+			break;
+		default:
+			ofst << "<Blank> ";
+			break;
+		}
 		ofst << "and it is flower: type = ";
 		switch (t) {
 		case 0:
